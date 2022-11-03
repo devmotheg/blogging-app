f176573cb442f411132a283494afcd0616a3d363
Mohamed Muntasir <devmotheg@gmail.com>
2022-11-03 07:58:05 +0300

    Fix an English grammar mistake...

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,27 +4,27 @@
  */
 
 const User = require("../models/user-model"),
-  handlerFactory = require("../utils/handler-factory"),
-  catchAsync = require("../utils/catch-async");
+	handlerFactory = require("../utils/handler-factory"),
+	catchAsync = require("../utils/catch-async");
 
 const factory = handlerFactory.useFactory(handlerFactory, User, "user");
 
 exports.passFilteredBody = operation => (req, res, next) => {
-  req.filteredBody = { ...req.body };
-  delete req.filteredBody.role;
+	req.filteredBody = { ...req.body };
+	delete req.filteredBody.role;
 
-  if (operation === "update") delete req.password;
+	if (operation === "update") delete req.password;
 
-  next();
+	next();
 };
 
 exports.updateMyInformation = catchAsync(async (req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, req.filteredBody);
+	await User.findByIdAndUpdate(req.user.id, req.filteredBody);
 
-  res.status(200).json({
-    status: "success",
-    message: "Your may use your new credentials anytime now",
-  });
+	res.status(200).json({
+		status: "success",
+		message: "You may use your new credentials anytime now",
+	});
 });
 
 exports.readAllUsers = factory("readAll");
